17fa5c0d5566c24f1a4468868f61eec60b2aa8da
Tunghs <juk08843@gmail.com>
2022-11-24 22:53:01 +0900

    * Fields 추가

--- MovingObjectDetection/ViewModel/MainViewModel.cs
+++ MovingObjectDetection/ViewModel/MainViewModel.cs
@@ -1,4 +1,6 @@
 using GalaSoft.MvvmLight;
+using MovingObjectDetection.Utils;
+using OpenCvSharp;
 using System.Windows.Media.Imaging;
 
 namespace MovingObjectDetection.ViewModel
@@ -6,11 +8,40 @@ namespace MovingObjectDetection.ViewModel
     public class MainViewModel : ViewModelBase
     {
         private BitmapImage _Image;
+        private BackgroundSubtractorKNN _mog;
+        Mat _remove = new Mat();
+
         public BitmapImage Image
         {
             get { return _Image; }
             set { Set(ref _Image, value); }
         }
+
+        private int _Exposure;
+        public int Exposure
+        {
+            get { return _Exposure; }
+            set
+            {
+                _Exposure = value;
+                _CamModule?.SetImageDeviceParam(Exposure, Focus);
+                RaisePropertyChanged("Exposure");
+            }
+        }
+
+        private int _Focus;
+        public int Focus
+        {
+            get { return _Focus; }
+            set
+            {
+                _Focus = value;
+                _CamModule?.SetImageDeviceParam(Exposure, Focus);
+                RaisePropertyChanged("Focus");
+            }
+        }
+
+        private WebCamModule _CamModule;
 
         public MainViewModel()
         {
